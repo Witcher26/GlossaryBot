6acb39b00c1b25638fd9e48191f8abca7f4f7e52
Witcher26 <extreme8igor088@mail.ru>
2022-08-13 23:53:13 +0300

    Fix 0.3.1: добавлен тестовый метод получения новостей с ленты.ру

---
+++ src/main/java/com/zvezdilin/TelegramBot/MainNews.java
@@ -0,0 +1,66 @@
+package com.zvezdilin.TelegramBot;
+
+import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.UpdatesListener;
+import com.pengrad.telegrambot.request.SendMessage;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class MainNews {
+    public static void main(String[] args) {
+        System.out.println("Telegram bot");
+
+        TelegramBot bot = new TelegramBot("5586945414:AAGHN0R9ybKOhKQFr1oVEcPLklwyDYHddXc");
+
+        bot.setUpdatesListener(updates -> {
+            while (true) {
+                updates.forEach(upd -> {
+                    try {
+                        System.out.println(upd);
+                        long chatId = upd.message().chat().id();
+                        String incomeMessage = upd.message().text();
+                        //logic
+                        String result = "";
+                        if (incomeMessage.equals("/start")) {
+                            result = "Hello " + upd.message().from().username() + ". ¬ыбери категорию: 1 - курсы валют, 2 - новости";
+                        }
+
+                        if (incomeMessage.equals("1")) {
+                            Document doc = Jsoup.connect("https://www.cbr.ru/scripts/XML_daily.asp?date_req=" + incomeMessage).get();
+                            System.out.println(doc.title());
+                            Elements valutes = doc.select("Valute");
+                            for (Element valute : valutes) {
+                                if (valute.attr("ID").equals("R01235")) {
+                                    result = valute.select("Value").text();
+                                    System.out.println(result);
+                                }
+                            }
+                        }
+                        if (incomeMessage.equals("2")) {
+                            int number = Integer.parseInt(incomeMessage);
+                            Document doc = Jsoup.connect("https://lenta.ru/rss").get();
+                            int index = number - 1;
+                            Element news = doc.select("item").get(index);
+                            String category = news.select("category").text();
+                            String title = news.select("title").text();
+                            String link = news.select("link").text();
+                            String description = news.select("description").text();
+                            result = category + "\n" + title + "\n" + description + "\n" + link;
+                        }
+                        // send response
+
+                        SendMessage request = new SendMessage(chatId, result);
+
+                        bot.execute(request);
+
+                    } catch (Exception ex) {
+                        ex.printStackTrace();
+                    }
+                });
+                return UpdatesListener.CONFIRMED_UPDATES_ALL;
+            }
+        });
+    }
+}
